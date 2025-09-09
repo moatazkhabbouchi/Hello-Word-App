251585f239283486bf23e6b167292d66140cc575
Moataz Khabbouchi <mootazkhabbouchi8@gmail.com>
2025-09-09 10:47:28 +0100

    added test comment

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from flask import Flask
 # Flask constructor takes the name of
 # current module (__name__) as argument.
 app = Flask(__name__)
- 
+ #test comment
 # The route() function of the Flask class is a decorator,
 # which tells the application which URL should call
 # the associated function.
